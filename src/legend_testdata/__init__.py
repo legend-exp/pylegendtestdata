1c33485eeeab4be412949d1821e2349e09a9a8b1
Luigi Pertoldi <luigi.pertoldi@protonmail.com>
2022-04-29 11:27:34 +0200

    First commit

---
+++ src/legend_testdata/__init__.py
@@ -0,0 +1,5 @@
+"""
+"""
+
+from legend_testdata._version import version as __version__
+from legend_testdata.core import get_path
---
+++ src/legend_testdata/core.py
@@ -0,0 +1,30 @@
+from git import (Repo, InvalidGitRepositoryError)
+import os
+import os.path as path
+
+
+def _get_testdata_repo():
+
+    repo_path = '/tmp/legend-testdata'
+
+    if not path.isdir(repo_path):
+        os.mkdir(repo_path)
+
+    repo = None
+    try:
+        repo = Repo(repo_path)
+    except InvalidGitRepositoryError:
+        repo = Repo.clone_from('https://github.com/legend-exp/legend-testdata', repo_path)
+
+    return repo_path, repo
+
+
+def get_path(filename: str):
+
+    rootdir = _get_testdata_repo()[0]
+    full_path = path.abspath(path.join(rootdir, 'data', filename))
+
+    if not path.isfile(full_path):
+        raise FileNotFoundError(f"Test file '{filename}' not found in legend-testdata repository")
+
+    return full_path
