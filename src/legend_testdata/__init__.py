3a3497e29cb4d8a21baaa0ec117f8caa47c9c553
Luigi Pertoldi <luigi.pertoldi@protonmail.com>
2023-05-15 08:56:01 +0200

    Add deprecation warning

--- src/legend_testdata/__init__.py
+++ src/legend_testdata/__init__.py
@@ -4,3 +4,8 @@ from legend_testdata._version import version as __version__
 from legend_testdata.core import LegendTestData
 
 __all__ = ["__version__", "LegendTestData"]
+
+import logging
+log = logging.getLogger(__name__)
+log.warning("the legend_testdata module has been renamed to legendtestdata, "
+            "install pylegendtestdata and import legendtestdata to suppress this warning")
